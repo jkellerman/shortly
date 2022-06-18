6a2e3926b0bf3d35b1c0505c427541217852457c
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-06-19 00:42:45 +0100

    add cta section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GlobalStyles from "./components/Global.styles";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import LinkShortener from "./components/LinkShortener";
+import CTA from "./components/CTA";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <LinkShortener />
       <FeatureSection />
+      <CTA />
     </>
   );
 };
---
+++ src/components/CTA.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { device } from "./Global.styles";
+import Button from "./Button";
+
+const CTAContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  background-image: url("../images/bg-boost-mobile.svg");
+  background-repeat: no-repeat;
+  background-position: top right;
+  background-size: cover;
+  background-color: #3a3054;
+  padding: 5.625rem 0;
+
+  @media (${device.tablet}) {
+    gap: 2rem;
+    background-image: url("../images/bg-boost-desktop.svg");
+  }
+`;
+
+const StyledCTA = styled.div`
+  font-size: 1.75rem;
+  font-weight: 700;
+  color: #fff;
+
+  @media (${device.laptop}) {
+    font-size: 2.5rem;
+  }
+`;
+
+const CTA = () => {
+  return (
+    <CTAContainer>
+      <StyledCTA>Boost your links today</StyledCTA>
+      <Button primary cta>
+        Get Started
+      </Button>
+    </CTAContainer>
+  );
+};
+
+export default CTA;
